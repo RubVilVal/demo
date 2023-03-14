18eeb7949bb24de6785bb2c7a650ed77cef698ac
Rubén <rubenvv97@gmail.com>
2023-03-14 14:41:41 +0100

    SQL script updated

--- sql_scripts/fill_database_Script.sql
+++ sql_scripts/fill_database_Script.sql
@@ -2,7 +2,7 @@ insert INTO stations (id, station_name)
 VALUES (1, 'Granada'), (2, 'Madrid');
 
 insert into passengers (id, name, surname, date_of_birth, password, role)
-values (1, 'Ruben', 'Vilchez', '1997-04-19', 'Rubenpass', 'ROLE_USER'), (2, 'Jane', 'Doe', '1986-06-08', 'Janepass', 'ROLE_USER'), (3, 'admin', 'admin', '2001-01-01', 'adminpass', 'ROLE_ADMIN');
+values (1, 'Ruben', 'Vilchez', '1997-04-19', '$2a$12$xsuYbJzndMsN2vVqKPofIe5RHRIoyt4A1dV6U6zt0.gWB.o4aWrNe', 'ROLE_USER'), (2, 'Jane', 'Doe', '1986-06-08', '$2a$12$KUUcooC9gvjR8MaBJyWayOFxrUDnZIcf12lXSWKqGTzRl4xaFBcEm', 'ROLE_USER'), (3, 'admin', 'admin', '2001-01-01', '$2a$12$3D8AYq0nh.DHBftGNV8ZxedPxxv2R6D70y3P91Md6VjG5PtjPk9Am', 'ROLE_ADMIN');
 
 insert into trains (number, seats)
 values (1, 12), (2, 30);
--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,11 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.entities.Passenger;
-import com.example.demo.services.PassengerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
